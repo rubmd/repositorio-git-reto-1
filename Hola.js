75127216ceb217fe9cae0593ad91215fcc3273be
rubmd <ruben.daza.2002@gmail.com>
2022-10-07 17:00:53 -0500

    se agrego una nueva linea de codigo a Mundo.js y se añadio un ; que hacia falta en el codigo. Ademas se creo un nuevo archivo llamado Final.js

--- Hola.js
+++ Hola.js
@@ -1,1 +1,2 @@
-console.log("Hola, soy el archivo Hola.js")
+console.log("Hola, soy el archivo Hola.js");
+console.log("Soy una modificacion del archivo original");
--- Mundo.js
+++ Mundo.js
@@ -1,1 +1,1 @@
-console.log("Hola, soy el archivo Mundo.js")
+console.log("Hola, soy el archivo Mundo.js");
